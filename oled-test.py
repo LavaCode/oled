33306a99c91e81ec98867a596443cfb0350eddce
Kasper <kasper.zegel@gmail.com>
2024-12-05 17:02:05 +0100

    repaired test

--- oled-test.py
+++ oled-test.py
@@ -16,7 +16,7 @@ import busio
 from adafruit_ssd1306 import SSD1306_I2C
 from PIL import Image, ImageDraw, ImageFont
 
-i2c = busio.I2C(board.SDA, board.SCL)
+i2c = busio.I2C(board.SCL, board.SDA)
 oled = SSD1306_I2C(128, 64, i2c, addr=0x3d)
 
 oled.fill(0)
